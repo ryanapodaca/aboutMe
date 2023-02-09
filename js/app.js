1694c0f319835c685264c2a184b89ca42651176e
DARSHON CRUDUP <dlc756@yahoo.com>
2023-02-09 17:50:56 -0500

    added function

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ let username = prompt('What is your name?');
 console.log('name is' + username);
 alert('Hello,' + username);
 
+function QuestionOne(){
 let q1 = prompt('Do I live in Hawaii?').toLowerCase();
 if (q1 === 'yes' || q1 === 'y'){
   //console.log('q1 correct');
@@ -15,7 +16,10 @@ if (q1 === 'yes' || q1 === 'y'){
   //console.log('q1 incorrect');
   alert('Sorry, you\'re incorrect');
 }
+QuestionOne();
+}
 
+function QuestionTwo(){
 let q2 = prompt('Do I like pizza?').toLowerCase();
 if (q2 === 'yes' || q2 === 'y'){
   //console.log('q2 correct');
@@ -25,7 +29,10 @@ if (q2 === 'yes' || q2 === 'y'){
   //console.log('q2 incorrect');
   alert('Sorry, you\'re incorrect');
 }
+QuestionTwo();
+}
 
+function QuestionThree(){
 let q3 = prompt('Do I like running?').toLowerCase();
 if (q3 === 'no' || q3 === 'n'){
   //console.log('q3 correct');
@@ -35,7 +42,10 @@ if (q3 === 'no' || q3 === 'n'){
   //console.log('q3 incorrect');
   alert('Sorry, you\'re incorrect');
 }
+QuestionThree();
+}
 
+function QuestionFour(){
 let q4 = prompt('Am I 32 years old?').toLowerCase();
 if (q4 === 'yes' || q4 === 'y'){
   //console.log('q4 correct');
@@ -45,7 +55,10 @@ if (q4 === 'yes' || q4 === 'y'){
   //console.log('q4 incorrect');
   alert('Sorry, you\'re incorrect');
 }
+QuestionFour();
+}
 
+QuestionFive(){
 let q5 = prompt('Do I play guitar?').toLowerCase();
 if (q5 === 'yes' || q5 === 'y') {
   //console.log('q5 correct');
@@ -54,6 +67,8 @@ if (q5 === 'yes' || q5 === 'y') {
 } else if (q5 === 'no' || q5 === 'n') {
   //console.log('q5 incorrect');
   alert('Sorry, you\'re incorrect');
+}
+QuestionFive();
 }
 
 function guessingGame(a){
